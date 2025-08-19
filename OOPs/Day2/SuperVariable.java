7dfbc8ab7a325edd1a27e70bd991cb3343775019
Janasrivvv <114650278+Janasrivvv@users.noreply.github.com>
2025-08-19 20:55:27 +0530

    Create SuperVariable.java

---
+++ OOPs/Day2/SuperVariable.java
@@ -0,0 +1,21 @@
+class Parent {
+    int value = 10;
+}
+
+class Child extends Parent {
+    int value = 20;
+
+    void display() {
+        System.out.println("Child value: " + value);
+        System.out.println("Parent value: " + super.value); // using super
+    }
+}
+
+public class SuperVariable {
+    public static void main(String[] args) {
+        new Child().display();
+    }
+}
+//
+Child value: 20
+Parent value: 10
